7f7ca8277d7473b4904aa78ea69787177030c9af
jashjchoi <jashjchoi@gmail.com>
2021-05-17 15:47:50 +0000

    Debugging task 3 files

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,9 +1,20 @@
 /*
    Checkbox for the list of amenities
 */
-const $ = window.$;
+console.log('document');
 
 $(document).ready(() => {
+  /*
+    Task 3: Request API
+  */
+  console.log('docs');
+  $.get('http://localhost:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
   const amenList = {};
   $('input[type=checkbox]').on('click', function () {
     if ($(this).prop('checked') === true) {
@@ -15,16 +26,6 @@ $(document).ready(() => {
       $('.amenities H4').html(Object.values(amenList).join(', ') + '&nbsp;');
     } else {
       $('.amenities H4').html('&nbsp;');
-    }
-  });
-  /*
-    Task 3: Request API
-*/
-  $.get('http://localhost:5001/api/v1/status/', function (data) {
-    if (data.status === 'OK') {
-      $('#api_status').addClass('available');
-    } else {
-      $('#api_status').removeClass('available');
     }
   });
 });
